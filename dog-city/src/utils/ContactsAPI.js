b7495c8d922f08593905b3d444cb7b642c78871a
Vikas Pandey <vikpande@gmail.com>
2017-11-05 22:41:01 +0100

    update of environment variables and deployment of location server to zeit

--- dog-city/src/utils/ContactsAPI.js
+++ dog-city/src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'https://contacts-server-fbvlerenda.now.sh'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
 
 let token = localStorage.token
 
